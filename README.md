cf08d4fb0e1a7895a8b86ece61bf6057332d0647
blackfox <blackfoxgamingstudio@gmail.com>
2019-06-29 01:58:01 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Drag-n-Drop Layout Editor Prototype
+
+> Made with Vue-Grid-Layout and Vue Js
+
+This mini project serves as an experiment for bitesize eLearning platform, [Pariksha](https://www.pariksha.io/).
+
+### [Read supporting article on Medium](https://blog.prototypr.io/grid-layout-editor-for-vue-js-a-research-project-for-pariksha-io-e3445025d21e)
+
+## [Demo](http://vue-grid-layout.surge.sh/#/)
+
+![Image](https://cdn-images-1.medium.com/max/800/1*oNqKKJNYAy45vos1C0VJkg.png)
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+
+For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/components/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container">
+    <div style="width: 930px; margin: 0 auto;">
+      <GridView></GridView>
+      <div style="background: #FCE4EC; text-align: center; padding: 50px; margin: 50px 0; border-radius: 5px;">
+        <h4> A modern Study Guide <strong>Lexi-kun</strong> using <strong>OneForAll as a Backend</strong> </h4>
+        <div> Check out same project built with OneForAll
+           <strong><a href="http://oneforall-224822.appspot.com/projects/" target="_blank">BlackFox OneForAll</a></strong>
+        </div>
+        <div> This R&amp;D project is developed to test grid-layout library for 
+          <strong> <a href="http://oneforall-224822.appspot.com/Storyboards/5cdb3809cda9320ed07725d7/" target="_blank"> OneForAll StoryBoard </a></strong>.
+        </div>
+        <div> Next you can make a python library using <strong><a href="http://oneforall-224822.appspot.com/codes/test" target="_blank"> The StoryCode CLI </a></strong>. </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+import GridView from './editor/GridView'
+
+export default {
+  name: 'home',
+  components: { GridView },
+  methods: {
+    ...mapActions({
+      fetchResources: 'fetchResources'
+    })
+  },
+  created () {
+    this.fetchResources()
+  }
+}
+</script>
